e8001b3b4c5b5445d5b9a28b04f6668f78a06aa8
Mischa Taylor <mischa@misheska.com>
2013-09-10 12:22:38 -0400

    Iteration #2 - Refactor to attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-group "myface"
+group node[:myface][:group]
 
-user "myface" do
-  group "myface"
+user node[:myface][:user] do
+  group node[:myface][:group]
   system true
   shell "/bin/bash"
 end

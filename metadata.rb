8f0eae29218036fcd55cd50c872aea3c12324d2b
Mischa Taylor <mischa@misheska.com>
2013-09-10 13:29:41 -0400

    Iteration #6 - Version Bump and Production Deploy

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'myface'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures myface'
+name             "myface"
+maintainer       "Mischa Taylor"
+maintainer_email "mischa@misheska.com"
+license          "Apache 2.0"
+description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          "1.0.0"
 
 depends "apache2", "~> 1.7.0"

17d5c18db88108702de14a61691838e054fe82f4
Mischa Taylor <mischa@misheska.com>
2013-09-10 14:16:52 -0400

    Iteration #7 - Install MySQL

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "mischa@misheska.com"
 license          "Apache 2.0"
 description      "Installs/Configures myface"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "2.0.0"
 
 depends "apache2", "~> 1.7.0"
+depends "mysql", "~> 3.0.0"
---
+++ recipes/database.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: myface
+# Recipe:: database
+#
+# Copyright (C) 2012 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "mysql::server"

643730ad073170f94ce71999b62fdf55fa0b3bac
Mischa Taylor <mischa@misheska.com>
2013-09-10 12:46:39 -0400

    Iteration #4 - Add Content

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,30 @@ user node[:myface][:user] do
 end
 
 include_recipe "apache2"
+
+# disable default site
+apache_site "000-default" do
+  enable false
+end
+
+# create apache config
+template "#{node[:apache][:dir]}/sites-available/myface.conf" do
+  source "apache2.conf.erb"
+  notifies :restart, 'service[apache2]'
+end
+
+# create document root
+directory "/srv/apache/myface" do
+  action :create
+  recursive true
+end
+
+# write site
+cookbook_file "/srv/apache/myface/index.html" do
+  mode "0644" # forget me to create debugging exercise
+end
+
+# enable myface
+apache_site "myface.conf" do
+  enable true
+end

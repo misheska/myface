d95938f71620980c0b9c052edbf2c0f49ea83c05
Mischa Taylor <misheska@getchef.com>
2013-12-28 14:50:56 -0800

    Iteration #5 - Refactoring webserver

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,45 +1,9 @@
-#
 # Cookbook Name:: myface
 # Recipe:: default
 #
 # Copyright (C) 2013 YOUR_NAME
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
 
-group node['myface']['group']
-
-user node['myface']['user'] do
-  group node['myface']['group']
-  system true
-  shell '/bin/bash'
-end
-
-include_recipe 'apache2'
-
-# disable default site
-apache_site '000-default' do
-  enable false
-end
-
-# create apache config
-template "#{node['apache']['dir']}/sites-available/myface.conf" do
-  source 'apache2.conf.erb'
-  notifies :restart, 'service[apache2]'
-end
-
-# create document root
-directory '/srv/apache/myface' do
-  action :create
-  recursive true
-end
-
-# write site
-cookbook_file '/srv/apache/myface/index.html' do
-  mode '0644' # forget me to create debugging exercise
-end
-
-# enable myface
-apache_site 'myface.conf' do
-  enable true
-end
+include_recipe 'myface::webserver'
---
+++ recipes/webserver.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: myface
+# Recipe:: webserver
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+group node['myface']['group']
+
+user node['myface']['user'] do
+  group node['myface']['group']
+  system true
+  shell '/bin/bash'
+end
+
+include_recipe 'apache2'
+
+# disable default site
+apache_site '000-default' do
+  enable false
+end
+
+# create apache config
+template "#{node['apache']['dir']}/sites-available/#{node['myface']['config']}" do
+  source 'apache2.conf.erb'
+  notifies :restart, 'service[apache2]'
+end
+
+# create document root
+directory "#{node['myface']['document_root']}" do
+  action :create
+  recursive true
+end
+
+# write site
+cookbook_file "#{node['myface']['document_root']}/index.html" do
+  mode '0644'
+end
+
+# enable myface
+apache_site "#{node['myface']['config']}" do
+  enable true
+end

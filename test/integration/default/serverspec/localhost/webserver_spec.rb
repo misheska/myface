ed30b282ffa2334700a6e9f93cd3eef2e10ca9c8
Mischa Taylor <misheska@getchef.com>
2013-12-29 14:21:07 -0800

    Iteration #17 - Completing the webserver test suite

--- test/integration/default/serverspec/localhost/webserver_spec.rb
+++ test/integration/default/serverspec/localhost/webserver_spec.rb
@@ -3,7 +3,22 @@ require 'spec_helper'
 describe 'MyFace webserver' do
 
   it 'should have a myface user' do
-    expect(command 'getent passwd myface').to return_stdout /myface:x:\d+:\d+::\/home\/myface:\/bin\/bash/
+    expect(user 'myface').to exist
+  end
+
+  it 'should be running the httpd server' do
+    case RSpec.configuration.os[:family]
+    when "Ubuntu"
+      expect(service 'apache2').to be_running
+      expect(service 'apache2').to be_enabled
+    else
+      expect(service 'httpd').to be_running
+      expect(service 'httpd').to be_enabled
+    end
+  end
+
+  it 'should respond to an HTTP request' do
+    expect(command 'curl localhost').to return_stdout /.*<title>MyFace Users<\/title>.*/
   end
 
 end
